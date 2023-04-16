f4df098ff4856e7f99690faaf48026fd22b212ad
Matej Bačo <matejbaco2000@gmail.com>
2023-04-16 20:51:22 +0200

    Fix SSR cookies

--- src/app/page-data.ts
+++ src/app/page-data.ts
@@ -1,23 +1,26 @@
 import "server-only";
 import { AppwriteProject, AppwriteService } from "./AppwriteService";
-import { cookies } from 'next/headers';
+import { cookies } from "next/headers";
 
 export async function getAccount() {
   const sessionNames = [
-		'a_session_' + AppwriteProject.toLowerCase(),
-		'a_session_' + AppwriteProject.toLowerCase() + '_legacy'
-	];
+    "a_session_" + AppwriteProject.toLowerCase(),
+    "a_session_" + AppwriteProject.toLowerCase() + "_legacy",
+  ];
 
   const cookieStore = cookies();
-	const hash = cookieStore.get(sessionNames[0]) ?? cookieStore.get(sessionNames[1]) ?? '';
-	AppwriteService.setSession(hash.toString());
+  const hash =
+    cookieStore.get(sessionNames[0]) ??
+    cookieStore.get(sessionNames[1]) ??
+    null;
+  AppwriteService.setSession(hash ? hash.value : "");
 
-	let account;
-	try {
-		account = await AppwriteService.getAccount();
-	} catch (err) {
-		account = null;
-	}
+  let account;
+  try {
+    account = await AppwriteService.getAccount();
+  } catch (err) {
+    account = null;
+  }
 
-	return account;
+  return account;
 }
