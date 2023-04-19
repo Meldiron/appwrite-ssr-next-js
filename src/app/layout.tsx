8e4580eb2b82e75399f912920d281c17c799cbcf
Matúš Ferčák <74977647+MatusFercak@users.noreply.github.com>
2023-04-19 22:30:29 +0200

    patch favIcon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,49 +2,27 @@ import "./globals.css";
 import "@appwrite.io/pink/dist/pink.css";
 import "@appwrite.io/pink-icons/dist/icon.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <link rel="icon" href="/favicon.png" />
       <meta name="viewport" content="width=device-width" />
 
       <title>Almost SSR | Next.js</title>
-      <meta
-        name="description"
-        content="Appwrite Loves Next.js! Demo application with authorized server-side and client-side rendering."
-      />
+      <meta name="description" content="Appwrite Loves Next.js! Demo application with authorized server-side and client-side rendering." />
 
       <meta property="og:url" content="https://next-js.ssr.almostapps.eu/" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Almost SSR | Next.js" />
-      <meta
-        property="og:description"
-        content="Appwrite Loves Next.js! Demo application with authorized server-side and client-side rendering."
-      />
-      <meta
-        property="og:image"
-        content="https://next-js.ssr.almostapps.eu/cover.png"
-      />
+      <meta property="og:description" content="Appwrite Loves Next.js! Demo application with authorized server-side and client-side rendering." />
+      <meta property="og:image" content="https://next-js.ssr.almostapps.eu/cover.png" />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="next-js.ssr.almostapps.eu" />
-      <meta
-        property="twitter:url"
-        content="https://next-js.ssr.almostapps.eu/"
-      />
+      <meta property="twitter:url" content="https://next-js.ssr.almostapps.eu/" />
       <meta name="twitter:title" content="Almost SSR | Next.js" />
-      <meta
-        name="twitter:description"
-        content="Appwrite Loves Next.js! Demo application with authorized server-side and client-side rendering."
-      />
-      <meta
-        name="twitter:image"
-        content="https://next-js.ssr.almostapps.eu/cover.png"
-      />
+      <meta name="twitter:description" content="Appwrite Loves Next.js! Demo application with authorized server-side and client-side rendering." />
+      <meta name="twitter:image" content="https://next-js.ssr.almostapps.eu/cover.png" />
 
       <body className="theme-dark">{children}</body>
     </html>
